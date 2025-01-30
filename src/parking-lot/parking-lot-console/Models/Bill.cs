d08a9c290c25291b579ac02708557bf539465efc
Bhoopendra Sharma <bhoopendras73@gmail.com>
2025-01-31 00:02:42 +0530

    parking lot - bill and payment models

--- src/parking-lot/parking-lot-console/Models/Bill.cs
+++ src/parking-lot/parking-lot-console/Models/Bill.cs
@@ -2,5 +2,12 @@
 {
     public class Bill : BaseModel
     {
+        public required string InvoiceNumber { get; set; }
+        public int Amount { get; set; }
+        public required Gate ExitGate { get; set; }
+        public DateTime ExitTime { get; set; }
+        public required Operator Operator { get; set; }
+        public required Ticket Ticket { get; set; }
+        public required List<Payment> Payments { get; set; }
     }
 }
---
+++ src/parking-lot/parking-lot-console/Models/Payment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parking_lot_console.Models
+{
+    public class Payment : BaseModel
+    {
+        public string TransactionId { get; set; }
+        public PaymentStatus PaymentStatus { get; set; }
+        public PaymentMode PaymentMode { get; set; }
+        public DateTime TimeOfPayment { get; set; }
+
+    }
+}
